3536648e79f6cc1bd101666501d3bbb32e793d15
Watieri <willywanzete@gmail.com>
2023-02-16 13:41:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistdio.h>
+#include <unistd.h>
 /**
  * main - A c program that prints a line to the starndad erro
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(1, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
