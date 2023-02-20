59960c45ebdc8ff5c7c889447e3e202f1eeaa2b3
Watieri <willywanzete@gmail.com>
2023-02-19 21:17:03 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * mainu - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
