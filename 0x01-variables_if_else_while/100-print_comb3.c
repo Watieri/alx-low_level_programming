e3e96588ff88f372bac65584f8e571a3ee3a2bd2
Watieri <willywanzete@gmail.com>
2023-02-19 21:52:12 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (m = 49; m <= 57; m++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
