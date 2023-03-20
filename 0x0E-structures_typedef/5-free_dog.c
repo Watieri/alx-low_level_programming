5aeb51812546bb3c933e161feabf1c7a949e37eb
Watieri <willywanzete@gmail.com>
2023-03-20 14:57:50 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
