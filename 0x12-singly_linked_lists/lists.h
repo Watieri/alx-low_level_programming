94c238d1f36353c6966a15aebc88341dec80de8a
Watieri <willywanzete@gmail.com>
2023-03-30 03:40:56 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LINKED
+#define LINKED
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s -singly linked list
@@ -12,9 +16,9 @@
  */
 typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
+	 char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -23,5 +27,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-endif
+endif /*LINKED*/
 
